ccda593d73bccabf641c61f8176b666a87b9363a
bruce <brucewa@ataarobotics.ca>
2022-12-10 11:25:46 -0700

    messing with imu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Oct29.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Oct29.java
@@ -6,6 +6,18 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 @TeleOp(name="Oct29", group="TeleOp")
 
 public class Oct29 extends LinearOpMode {
@@ -30,13 +42,19 @@ public class Oct29 extends LinearOpMode {
     public boolean bool = toggle;
     public boolean val = false;
 
+    BNO055IMU               imu;
+    Orientation             lastAngles = new Orientation();
+    double                  globalAngle, power = .30, correction;
+
 
 
     @Override
     public void runOpMode() throws InterruptedException {
-        robotui = new DemoRobotInterface(hardwareMap, telemetry);
 
-        waitForStart();
+        telemetry.addData("Mode", "starting...");
+        telemetry.update();
+
+        robotui = new DemoRobotInterface(hardwareMap, telemetry);
 
         robotui.clawS.setDirection(Servo.Direction.REVERSE);
 
@@ -45,9 +63,48 @@ public class Oct29 extends LinearOpMode {
 
 
 
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+
+        parameters.mode                = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled      = false;
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        imu.initialize(parameters);
+
+        telemetry.addData("Mode", "calibrating...");
+        telemetry.update();
+
+        // make sure the imu gyro is calibrated before continuing.
+        while (!isStopRequested() && !imu.isGyroCalibrated())
+        {
+            sleep(50);
+            idle();
+        }
+
+        telemetry.addData("Mode", "waiting for start");
+        telemetry.addData("imu calib status", imu.getCalibrationStatus().toString());
+        telemetry.update();
+
+
+        // wait for start button.
+
+        waitForStart();
+
+        telemetry.addData("Mode", "running");
+        telemetry.update();
 
         while (opModeIsActive())
         {
+            lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            telemetry.addData("imu ", lastAngles.toString());
+            telemetry.update();
+
             if(gamepad1.left_trigger > 0.5)
             {
                 axisX = clamp(-gamepad1.left_stick_y, -boostOff, boostOff);
