67f66f166807938e711ce90f339f7fb64e879907
bruce <brucewa@ataarobotics.ca>
2022-12-10 12:07:25 -0700

    tweaks

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Oct29.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Oct29.java
@@ -18,10 +18,10 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
-@TeleOp(name="Oct29", group="TeleOp")
+@TeleOp(name="IMUfun", group="TeleOp")
 
-public class Oct29 extends LinearOpMode {
-    private DemoRobotInterface robotui = null;
+public class IMUfun extends LinearOpMode {
+//    private DemoRobotInterface robotui = null;
     public float axisX;
     public float axisY;
 
@@ -53,14 +53,6 @@ public class Oct29 extends LinearOpMode {
 
         telemetry.addData("Mode", "starting...");
         telemetry.update();
-
-        robotui = new DemoRobotInterface(hardwareMap, telemetry);
-
-        robotui.clawS.setDirection(Servo.Direction.REVERSE);
-
-        robotui.frm.setDirection(DcMotorSimple.Direction.REVERSE);
-        robotui.flm.setDirection(DcMotorSimple.Direction.REVERSE);
-
 
 
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
@@ -104,107 +96,6 @@ public class Oct29 extends LinearOpMode {
             lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
             telemetry.addData("imu ", lastAngles.toString());
             telemetry.update();
-
-            if(gamepad1.left_trigger > 0.5)
-            {
-                axisX = clamp(-gamepad1.left_stick_y, -boostOff, boostOff);
-                axisY = clamp(-gamepad1.left_stick_x, -boostOff, boostOff);
-            }
-            else if(gamepad1.right_trigger > 0.5)
-            {
-                axisX = clamp(-gamepad1.left_stick_y, -highBoost, highBoost);
-                axisY = clamp(-gamepad1.left_stick_x, -highBoost, highBoost);
-            }
-            else
-            {
-                axisX = clamp(-gamepad1.left_stick_y, -lowBoost, lowBoost);
-                axisY = clamp(-gamepad1.left_stick_x, -lowBoost, lowBoost);
-            }
-
-
-            if((axisX <0.1)&&(axisX>-0.1)) // turn
-            {
-                robotui.flm.setPower(-axisY);
-                robotui.frm.setPower(-axisY);
-            }
-
-            else if((axisY <0.1)&&(axisY>-0.1)) // go fwrd/bckwrds
-            {
-                robotui.flm.setPower(axisX);
-                robotui.frm.setPower(axisX*-1);
-            }
-
-
-            if(gamepad2.y)
-            {
-                robotui.elevatorm.setTargetPosition(highPos);
-                robotui.elevatorm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                robotui.elevatorm.setPower(speed);
-            }
-
-            if(gamepad2.b)
-            {
-                robotui.elevatorm.setTargetPosition(medPos);
-                robotui.elevatorm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                robotui.elevatorm.setPower(speed);
-            }
-
-            if(gamepad2.a)
-            {
-                robotui.elevatorm.setTargetPosition(lowPos);
-                robotui.elevatorm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                robotui.elevatorm.setPower(speed);
-            }
-
-            if(gamepad2.right_bumper)
-            {
-                robotui.elevatorm.setTargetPosition(limitDN);
-                robotui.elevatorm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                robotui.elevatorm.setPower(speed);
-            }
-
-            if(gamepad2.x)
-            {
-                robotui.elevatorm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            }
-
-            if(robotui.elevatorm.getMode() == DcMotor.RunMode.RUN_WITHOUT_ENCODER)
-            {
-
-                if(gamepad2.dpad_up && robotui.elevatorm.getCurrentPosition() < limitUP)
-                {
-                    robotui.elevatorm.setPower(speed);
-                }
-                else if(gamepad2.dpad_down && robotui.elevatorm.getCurrentPosition() > limitDN)
-                {
-                    robotui.elevatorm.setPower(-speed*2.5);
-                }
-                else
-                {
-                    robotui.elevatorm.setPower(0);
-                }
-            }
-
-            if(robotui.elevatorm.getCurrentPosition() == robotui.elevatorm.getTargetPosition())
-            {
-                robotui.elevatorm.setPower(0);
-            }
-
-
-            if(gamepad2.left_trigger > 0.2)
-            {
-                robotui.clawF.setPosition(0.05);
-                robotui.clawS.setPosition(0.05);
-            }
-            else if(gamepad2.right_trigger < 0.2)
-            {
-                robotui.clawF.setPosition(-0.1);
-                robotui.clawS.setPosition(-0.1);
-            }
         }
-    }
-
-    public static float clamp(final float value, final float min, final float max) {
-        return (value < min) ? min : ((value > max) ? max : value);
     }
 }
