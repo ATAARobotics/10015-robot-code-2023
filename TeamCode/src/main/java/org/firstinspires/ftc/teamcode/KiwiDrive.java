5c7c4b6f3b5667287d4f812198da4a185d7c5027
meejah <meejah@meejah.ca>
2023-01-21 11:34:38 -0700

    refactor: move elevator code to custom class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KiwiDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KiwiDrive.java
@@ -35,44 +35,24 @@ import com.arcrobotics.ftclib.gamepad.ButtonReader;
 import com.arcrobotics.ftclib.gamepad.TriggerReader;
 import com.arcrobotics.ftclib.controller.PIDController;
 
+// our other classes, for 10015
 import org.firstinspires.ftc.teamcode.DriveBase;
+import org.firstinspires.ftc.teamcode.Elevator;
+
 
 @TeleOp(name="Kiwi: OpMode", group="Opmode")
 public class KiwiDrive extends OpMode {
 
     private DriveBase drivebase = null;
+    private Elevator elevator = null;
+
+    private GamepadEx gamepadex1 = null;
+    private GamepadEx gamepadex2 = null;
 
     private ColorSensor colour = null;
 
-    // claw + elevator setup
-    private Motor motor_elevator = null;
-    private Servo servo_claw_left = null;
-    private Servo servo_claw_right = null;
-
-    //servotoggle
-    // false is open true is closed
-    private boolean claw_position = false;
-
-    //eleator position
-    private int elevator_position = 0;
-
     // time-tracking
     private double last_time = 0.0;
-
-    // setup for various controls
-    private GamepadEx gamepadex1 = null;
-    private GamepadEx gamepadex2 = null;
-    private ButtonReader bump_left = null;
-    private ButtonReader bump_right = null;
-    private ButtonReader button_a = null;
-    private ButtonReader button_y = null;
-    private ButtonReader button_b = null;
-    private ButtonReader button_x = null;
-    private ButtonReader dpad_down = null;
-    private TriggerReader trigger_left = null;
-    private TriggerReader trigger_right = null;
-    private TriggerReader trigger_left2 = null;
-    private TriggerReader trigger_right2 = null;
 
     @Override
     public void init() {
@@ -81,30 +61,15 @@ public class KiwiDrive extends OpMode {
 
         //colour = hardwareMap.get(ColorSensor.class, "colour");
 
-        // initialize motors
-        motor_elevator = new Motor(hardwareMap, "hdelevator");
-
-        motor_elevator.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
-
-        motor_elevator.setRunMode(Motor.RunMode.PositionControl);
-        motor_elevator.setInverted(true); // so that "up" is positive for us
-        motor_elevator.set(0);
-        motor_elevator.setPositionCoefficient(0.02);
-
         // setup some controller listeners
         gamepadex1 = new GamepadEx(gamepad1);
         gamepadex2 = new GamepadEx(gamepad2);
 
-        // elevator / claw motors
-        servo_claw_left = hardwareMap.get(Servo.class, "hdclawleft");
-        servo_claw_right = hardwareMap.get(Servo.class, "hdclawright");
-        servo_claw_left.setDirection(Servo.Direction.FORWARD);
-        servo_claw_right.setDirection(Servo.Direction.FORWARD);
-        servo_claw_left.scaleRange(0.0, 1.0);
-        servo_claw_right.scaleRange(0.0, 1.0);
-
         // let the drivebase set itself up
         drivebase = new DriveBase(hardwareMap);
+
+        // elevator setup
+        elevator = new Elevator(hardwareMap);
 
         telemetry.addData("status", "initialized");
         telemetry.update();
@@ -123,7 +88,8 @@ public class KiwiDrive extends OpMode {
 
         // make sure robot starts at correct position
         drivebase.imu.resetYaw();
-        motor_elevator.resetEncoder();
+        // ensure we have "zero" at the bottom of our elevator
+        elevator.motor_elevator.resetEncoder();
     }
 
     @Override
@@ -136,128 +102,28 @@ public class KiwiDrive extends OpMode {
 
         telemetry.addData("time", time);
 
-        String detected_colour = "unknown";
-        int r = colour.red();
-        int g = colour.green();
-        int b = colour.blue();
-        int max = Math.max(colour.alpha(), Math.max(b, Math.max(r, g)));
-        if (r == max) { detected_colour = "red"; }
-        if (g == max) { detected_colour = "green"; }
-        if (b == max) { detected_colour = "blue"; }
-        telemetry.addData(
-            "colour",
-            String.format("colour: red=%d green=%d blue=%d max=%d detected=%s", r, g, b, max, detected_colour)
-        );
+        if (false) {
+            String detected_colour = "unknown";
+            int r = colour.red();
+            int g = colour.green();
+            int b = colour.blue();
+            int max = Math.max(colour.alpha(), Math.max(b, Math.max(r, g)));
+            if (r == max) { detected_colour = "red"; }
+            if (g == max) { detected_colour = "green"; }
+            if (b == max) { detected_colour = "blue"; }
+            telemetry.addData(
+                "colour",
+                String.format("colour: red=%d green=%d blue=%d max=%d detected=%s", r, g, b, max, detected_colour)
+            );
+        }
 
         // let FTCLib updates it's button status
         gamepadex1.readButtons();
         gamepadex2.readButtons();
 
-
-        // Elevator Controls (move to separate class)
-        elevator_position = motor_elevator.getCurrentPosition();
-        double elevator_speed = 0.65;
-
-        double elevator_high_limit = 1575;
-        double elevator_low_limit = 10;
-        telemetry.addData("elevator-encoder", motor_elevator.getCurrentPosition());
-        telemetry.addData("elevator-value", motor_elevator.get());
-        telemetry.addData("elevator", "unknown");
-
-        // elevator control
-
-        //elevator presets
-        if (false) {//gamepadex2.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER) < 0.5 && gamepadex2.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) < 0.5) {
-            //build-in P-controller presets
-            //low 700 meduim 1190 high1560 could go higher
-
-            motor_elevator.setRunMode(Motor.RunMode.PositionControl);
-
-            if (gamepadex2.isDown(GamepadKeys.Button.A)) {
-                motor_elevator.setTargetPosition(50);
-                telemetry.addData("elevator", "preset-bottom");
-            } else if (gamepadex2.isDown(GamepadKeys.Button.X)) {
-                motor_elevator.setTargetPosition(700);
-                telemetry.addData("elevator", "preset-short");
-            } else if (gamepadex2.isDown(GamepadKeys.Button.B)) {
-                motor_elevator.setTargetPosition(1190);
-                telemetry.addData("elevator", "preset-medium");
-            } else if (gamepadex2.isDown(GamepadKeys.Button.Y)) {
-                motor_elevator.setTargetPosition(1550);
-                telemetry.addData("elevator", "preset-tall");
-            } else {
-                motor_elevator.set(0);
-                telemetry.addData("elevator","stop");
-            }
-
-            // actually input the control to the target
-            if (!motor_elevator.atTargetPosition()) {
-                // XXX why does this ever successfuly go "down" at all??
-                motor_elevator.set(0.15);
-            }
-        } else {
-            // manual elevator control
-            // setting the speed to0.05 counteracts the pull of gravity
-            motor_elevator.setRunMode(Motor.RunMode.RawPower);
-            double left_stick = gamepadex2.getLeftY() * 1.0;
-//            if (gamepadex2.getLeftY() < 0.05 || gamepadex2.getLeftY() > 0.05) {
-//                right_stick = gamepadex2.getLeftY() * 0.5;
-//            }
-            if (left_stick > 0.05) {
-                motor_elevator.setRunMode(Motor.RunMode.RawPower);
-                if (motor_elevator.getCurrentPosition() < elevator_high_limit) {
-                    motor_elevator.set(elevator_speed * left_stick);
-                    telemetry.addData("elevator", "up");
-                }
-            } else if (left_stick < -0.05) {
-                motor_elevator.setRunMode(Motor.RunMode.RawPower);
-                if (motor_elevator.getCurrentPosition() > elevator_low_limit) {
-                    // go really slow if we're close to the bottom
-                    if (motor_elevator.getCurrentPosition() < 400) {
-                        motor_elevator.set(elevator_speed * left_stick * 0.2);
-                    } else {
-                        motor_elevator.set(elevator_speed * left_stick * 0.2);
-                    }
-                    telemetry.addData("elevator","down");
-                }
-            } else {
-                motor_elevator.setRunMode(Motor.RunMode.RawPower);
-                motor_elevator.set(0.0);
-                telemetry.addData("elevator","stop");
-            }
-        }
-
-        if (gamepadex2.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) >0.5 || gamepadex2.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER) > 0.5) {
-            motor_elevator.setTargetPosition(400);
-            motor_elevator.setRunMode(Motor.RunMode.PositionControl);
-            if (!motor_elevator.atTargetPosition()) {
-                // XXX why does this ever successfuly go "down" at all??
-                motor_elevator.set(0.1);
-            }
-        }
-        //claw controls
-        if (gamepadex2.wasJustPressed(GamepadKeys.Button.A) || gamepadex2.wasJustPressed(GamepadKeys.Button.X)) {
-            claw_position = !claw_position;
-            if (claw_position == true) {  // closed
-                servo_claw_left.setPosition(0.00);
-                servo_claw_right.setPosition(1.00);
-                //motor_elevator.set(0.5)for 1 second?
-            } else { // must be false (open)
-                servo_claw_left.setPosition(1.0);
-                servo_claw_right.setPosition(0.0);
-            }
-        }
-        telemetry.addData(
-            "servos",
-            String.format(
-                "claw %b left %.2f right %.2f",
-                claw_position,
-                servo_claw_left.getPosition(),
-                servo_claw_right.getPosition()
-            )
-        );
-
+        // tie controllers through to components
         drivebase.do_drive_updates(gamepadex1, telemetry);
+        elevator.do_elevator_updates(gamepadex2, telemetry);
 
         telemetry.update();
     }
