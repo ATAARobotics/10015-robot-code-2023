205b1f4e19894a03b81e129e3a02752266e60d5d
meejah <meejah@meejah.ca>
2023-02-07 20:49:26 -0700

    add, use OpenCV for QR code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCv.java
@@ -0,0 +1,209 @@
+package org.firstinspires.ftc.teamcode;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import com.arcrobotics.ftclib.command.button.Trigger;
+import com.arcrobotics.ftclib.gamepad.ToggleButtonReader;
+import com.arcrobotics.ftclib.gamepad.TriggerReader;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+
+import org.opencv.core.Mat;
+import org.opencv.objdetect.QRCodeDetector;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+import org.openftc.easyopencv.OpenCvInternalCamera2;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.hardware.motors.RevRoboticsUltraPlanetaryHdHexMotor;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+
+// this is just a dumb wrapper
+//import com.arcrobotics.ftclib.hardware.RevIMU;
+import com.arcrobotics.ftclib.drivebase.HDrive;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.RevIMU;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.gamepad.ButtonReader;
+import com.arcrobotics.ftclib.gamepad.TriggerReader;
+import com.arcrobotics.ftclib.controller.PIDController;
+
+// our other classes, for 10015
+import org.firstinspires.ftc.teamcode.DriveBase;
+import org.firstinspires.ftc.teamcode.Elevator;
+
+
+@Autonomous(name="Open CV", group="Autonomous")
+public class OpenCv extends LinearOpMode {
+
+    private DriveBase drivebase = null;
+    private Elevator elevator = null;
+    private GamepadEx gamepadex1 = null;
+
+    private ColorSensor colour = null;
+    private DistanceSensor distance = null;
+    private String found_colour = "unknown";
+
+
+    private void ensure_stop(double heading) {
+        /// ideally shouldn't need this, but .. here we are
+        drivebase.drive.driveFieldCentric(0.0, 0.0, 0.0, heading);
+        drivebase.motor_left.set(0.0);
+        drivebase.motor_right.set(0.0);
+        drivebase.motor_slide.set(0.0);
+    }
+
+    public class Action {
+        public double start_time = -1.0;
+
+        public void do_drive(double heading, DriveBase drivebase) {
+            drivebase.drive.driveFieldCentric(0.0, 0.0, 0.0, heading);
+        }
+
+        public void do_elevator(Elevator elevator) {
+            if (!elevator.motor_elevator.atTargetPosition()) {
+                elevator.motor_elevator.set(0.15);
+            }
+        }
+
+        public boolean is_done(double time) {
+            return false;
+        }
+    }
+
+    public class DriveAction extends Action {
+        public double stick_x = 0.0;
+        public double stick_y = 0.0;
+        public double turn = 0.0;
+        public double duration = 0.0;
+
+        public DriveAction(double x, double y, double t, double dur) {
+            stick_x = x;
+            stick_y = y;
+            turn = t;
+            duration = dur;
+        }
+
+        public void do_drive(double heading, DriveBase drivebase) {
+            drivebase.drive.driveFieldCentric(stick_x, stick_y, turn, heading);
+        }
+
+        public boolean is_done(double time) {
+            if ((time - start_time) > duration) {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    @Override
+    public void runOpMode() {
+        //
+        // setup
+        //
+
+        telemetry.addData("status", "startup");
+        telemetry.update();
+
+        colour = hardwareMap.get(ColorSensor.class, "colour");
+        colour.enableLed(true);
+
+        distance = hardwareMap.get(DistanceSensor.class, "colour");
+
+        // let the drivebase set itself up
+        drivebase = new DriveBase(hardwareMap);
+
+        // elevator setup
+        elevator = new Elevator(hardwareMap);
+
+        // ust one controller
+        gamepadex1 = new GamepadEx(gamepad1);
+
+        telemetry.addData("status", "initialized");
+        telemetry.update();
+
+        //
+        // done setup wait for start
+        //
+
+        waitForStart();
+
+        //
+        // start has been pressed
+        //
+
+        // make sure robot starts at correct position
+        drivebase.imu.resetYaw();
+        // ensure we have "zero" at the bottom of our elevator
+        elevator.motor_elevator.resetEncoder();
+        elevator.motor_elevator.setRunMode(Motor.RunMode.PositionControl);
+
+        //
+        // OpenCV stuff
+        //
+        OpenCvCamera camera = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK);
+        OpenCvCamera camera2 = OpenCvCameraFactory.getInstance().createInternalCamera2(OpenCvInternalCamera2.CameraDirection.BACK);
+
+        class FindQrCodePipeline extends OpenCvPipeline
+        {
+            public boolean found_qr = false;
+            public String found_name = "";
+            private QRCodeDetector qr = new QRCodeDetector();
+
+            @Override
+            public Mat processFrame(Mat input)
+            {
+                found_name = qr.detectAndDecode(input);
+                return input;
+            }
+        }
+
+        // note, synchronous open (can do async instead, then we don't block here)
+        camera.openCameraDevice();
+        camera.startStreaming(1920, 1080, OpenCvCameraRotation.SIDEWAYS_LEFT);
+        FindQrCodePipeline pipeline = new FindQrCodePipeline();
+        camera.setPipeline(pipeline);
+
+        while(opModeIsActive()) {
+            telemetry.addData(
+                "camera",
+                String.format(
+                    "fps=%.2f frametime=%.2f ms ",
+                    camera.getFps(),
+                    camera.getTotalFrameTimeMs()
+                )
+            );
+            telemetry.addData(
+                "qr",
+                String.format(
+                    "found=%s",
+                    pipeline.found_name
+                )
+            );
+            telemetry.update();
+        }
+    }
+}
